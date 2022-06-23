5e9491df1f59c1ec1b34fbab6acdcbc4b1668349
LFMP <luizfellipemachipereira@gmail.com>
2022-06-22 21:09:40 -0300

    add save to npz

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,24 +1,40 @@
 import os
 
+import cv2
+import numpy as np
 import pandas as pd
 
 
-def get_valid_cases(path,label):
+def get_valid_cases(path, label):
   """
   Get the valid cases (image with mask) from the given path.
   """
-  cases = {'patient': [],'image': [], 'mask': [], 'class': []}
+  cases = {
+      'patient': [],
+      'image_path': [],
+      'mask_path': [],
+      'flair': [],
+      'class': []
+  }
   for case in os.listdir(path):
-    if case.endswith('.bmp'):
-      cases['patient'].append(case.split('_')[0])
-      cases['class'].append(label)
-      cases['image'].append(case)
-      mask = case.replace('.bmp', '_mask.png')
-      if mask in os.listdir(path):
-        cases['mask'].append(mask)
-      else:
-        cases['mask'].append(None)
+    if 'mask' in case:
+      patient_id = case.split('_')[0]
+      flair = int(case.split('FLAIR')[1].split('_')[0])
+      possible_image_path = []
+      if flair < 10:
+        possible_image_path.append(f'{patient_id}_FLAIR0{flair}.bmp')
+      possible_image_path.append(case.replace('_mask.png', '.bmp'))
+      possible_image_path.append(case.replace('_mask.png', '.png'))
+      for pp in possible_image_path:
+        if pp in os.listdir(path):
+          cases['class'].append(label)
+          cases['patient'].append(patient_id)
+          cases['flair'].append(flair)
+          cases['mask_path'].append(os.path.join(path, case))
+          cases['image_path'].append(os.path.join(path, pp))
+          break
   return pd.DataFrame(cases)
+
 
 def split_dataset(df, split_ratio):
   """
@@ -27,6 +43,7 @@ def split_dataset(df, split_ratio):
   train_df = df.groupby('patient').sample(frac=split_ratio, random_state=42)
   val_df = df.drop(train_df.index)
   return train_df, val_df
+
 
 def get_dataset(path, split_ratio):
   """
@@ -40,3 +57,50 @@ def get_dataset(path, split_ratio):
   train_df = pd.concat([train_df for train_df, _ in datasets])
   val_df = pd.concat([val_df for _, val_df in datasets])
   return train_df, val_df
+
+
+def make_dataset(path):
+  """
+  Make the dataset from the given path.
+  """
+  train_df, val_df = get_dataset(os.path.join(path, 'raw'), 0.8)
+  train_df.to_csv(os.path.join(path, 'iterim', 'train.csv'), index=False)
+  val_df.to_csv(os.path.join(path, 'iterim', 'val.csv'), index=False)
+
+  x_train, Y_train, z_train = train_df['image_path'], train_df[
+      'class'], train_df['mask_path']
+  x_val, Y_val, z_val = val_df['image_path'], val_df['class'], val_df[
+      'mask_path']
+  X_train, X_val, Z_train, Z_val = [], [], [], []
+
+  for im_path in x_train:
+    X_train.append(np.array(cv2.imread(im_path, 0)))
+
+  for im_path in z_train:
+    im_mask = np.array(cv2.imread(im_path, cv2.IMREAD_UNCHANGED))
+    _, im_mask = cv2.threshold(im_mask, 0, 255,
+                               cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    Z_train.append(im_mask)
+
+  assert (len(X_train) == len(Z_train) == len(Y_train))
+
+  np.savez(os.path.join(path, 'iterim', 'train.npz'),
+           X_train=X_train,
+           Y_train=Y_train,
+           Z_train=Z_train)
+
+  for im_path in x_val:
+    X_val.append(np.array(cv2.imread(im_path, 0)))
+
+  for im_path in z_val:
+    im_mask = np.array(cv2.imread(im_path, cv2.IMREAD_UNCHANGED))
+    _, im_mask = cv2.threshold(im_mask, 0, 255,
+                               cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    Z_val.append(im_mask)
+
+  assert (len(X_val) == len(Z_val) == len(Y_val))
+
+  np.savez(os.path.join(path, 'iterim', 'val.npz'),
+           X_val=X_val,
+           Y_val=Y_val,
+           Z_val=Z_val)
